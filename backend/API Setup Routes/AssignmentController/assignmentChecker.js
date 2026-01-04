30d9c4c26f8a3c94cc905d8b477883d7ecc9399b
Aliimran1074 <aliimransachwani4@gmail.com>
2026-01-05 01:27:29 +0500

    little changes

--- backend/API Setup Routes/AssignmentController/assignmentChecker.js
+++ backend/API Setup Routes/AssignmentController/assignmentChecker.js
@@ -1,3 +1,4 @@
+const { pdfToImageBuffer } = require('../../PDFToImageToBlocks/pdfToImage')
 const {detectPdfFileType}= require('../AssignmentFileJustification/fileJustification')
 
 // const 
@@ -12,6 +13,13 @@ try {
     if(!data){
       return res.status(400).json({message:"Issue in Getting File Data"})
     }
+
+    const pdfType = data.type
+    if(pdfType=="MIXED" || pdfType=="SCANNED") {
+     const images =  await pdfToImageBuffer(pdfBuffer)
+    //  run logic to check assignment of pics
+     } console.log('PDF type:',data.type)
+     
     return res.status(200).json({message:"Data of PDF Found",data})
 } catch (error) {
   console.log(" Error in  Check PDF File Function",error)
--- backend/API Setup Routes/AssignmentFileJustification/fileJustification.js
+++ backend/API Setup Routes/AssignmentFileJustification/fileJustification.js
@@ -1,4 +1,5 @@
-const pdfjsLib = require('pdfjs-dist/legacy/build/pdf.js')
+const pdfjsLib = require('pdfjs-dist/legacy/build/pdf.js');
+const { text } = require('pdfkit');
 
 pdfjsLib.GlobalWorkerOptions.workerSrc= require.resolve("pdfjs-dist/legacy/build/pdf.worker.js")
 
@@ -17,9 +18,20 @@ let pageWithText=0
 for(let i=1;i<=pdfDoc.numPages;i++){
     const page = await pdfDoc.getPage(i)
     const textContent = await page.getTextContent()
-    const pageText = textContent.items.map(item=>item.str).join(" ").trim()
-    textPages.push(pageText)
-    const wordCount =pageText.split(/\s+/).length
+    const viewport= page.getViewport({scale:1})
+
+    const headerY=viewport.height*0.85
+    const footerY= viewport.height*0.15
+    const bodyText=textContent.items.filter(item=>{
+      const y = item.transform[5]
+      return y > footerY && y <headerY
+    })
+    .map(item=>item.str)
+    .join(" ")
+    .trim()
+    // const pageText = textContent.items.map(item=>item.str).join(" ").trim()
+    textPages.push(bodyText)
+    const wordCount =bodyText.split(/\s+/).length
     if(wordCount>10) pageWithText++
     // if(pageText.length>20) pageWithText++
 }
